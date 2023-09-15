b293762968225130b43ac79f767fb4f24a7eca06
Manuel Calvo <mmartinc@axpe.com>
2023-09-15 19:37:49 +0200

    add new name param #5

--- src/main/java/es/upm/miw/iwvg_devops/code/NewIssue.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/NewIssue.java
@@ -2,7 +2,9 @@ package es.upm.miw.iwvg_devops.code;
 
 public class NewIssue {
     private int issueId;
-    public NewIssue(int issueId) {
+    private String name;
+    public NewIssue(int issueId, String name) {
         this.issueId = issueId;
+        this.name = name;
     }
 }
