cb172b776a354d61e2f7852bfaeed9083b948878
Manuel Calvo <mmartinc@axpe.com>
2023-09-15 19:36:30 +0200

    fix casing #4

--- src/main/java/es/upm/miw/iwvg_devops/code/NewIssue.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/NewIssue.java
@@ -2,7 +2,7 @@ package es.upm.miw.iwvg_devops.code;
 
 public class NewIssue {
     private int issueId;
-    public NewIssue(int issueId) {
-        this.issueId = issueId;
+    public NewIssue(int IssueId) {
+        this.issueId = IssueId;
     }
 }
